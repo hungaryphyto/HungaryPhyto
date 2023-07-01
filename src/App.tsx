d2b6a8ed555f56a88035fe9c730d1847dfb360da
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-07-01 00:49:50 -0300

    fix: commented persistLogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import PersistLogin from "./components/redirects/PersistLogin.tsx";
+/* import PersistLogin from "./components/redirects/PersistLogin.tsx"; */
 import RedirectIfAuth from "./components/redirects/RedirectIfAuth.tsx";
 import RequireAuth from "./components/redirects/RequireAuth.tsx";
 import Layout from "./components/Layout.tsx";
